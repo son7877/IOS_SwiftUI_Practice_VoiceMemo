3c217fc09a745fd53903129eb008b5ff4eb12aac
HongBeom <hongbeom7877@naver.com>
2024-12-18 16:08:33 +0900

    [feat] #1 MemoViewModel

--- voiceMemo/Feature/MemoList/Memo/MemoView.swift
+++ voiceMemo/Feature/MemoList/Memo/MemoView.swift
@@ -6,6 +6,10 @@
 import SwiftUI
 
 struct MemoView: View {
+    @EnvironmentObject private var pathModel: PathModel
+    @EnvironmentObject private var memoListViewModel: MemoListViewModel
+    @StateObject var memoViewModel: MemoViewModel
+    @State var isCreateMode: Bool = true
  
   var body: some View {
     Text("Memo")
@@ -14,6 +18,14 @@ struct MemoView: View {
 
 struct MemoView_Previews: PreviewProvider {
   static var previews: some View {
-    MemoView()
+    MemoView(
+      memoViewModel: .init(
+        memo: .init(
+          title: "",
+          content: "",
+          date: Date()
+        )
+      )
+    )
   }
 }
