108da9a2824a6264e0b12a34e05d134d38e5b5f1
HongBeom <hongbeom7877@naver.com>
2024-12-21 16:27:56 +0900

    [feat] #2 VoiceRecorderView - TitleView

--- voiceMemo/Feature/VoiceRecorder/VoiceRecorderView.swift
+++ voiceMemo/Feature/VoiceRecorder/VoiceRecorderView.swift
@@ -6,9 +6,26 @@
 import SwiftUI
 
 struct VoiceRecorderView: View {
-  var body: some View {
-    Text("VoiceRecorder")
-  }
+    @StateObject private var viewModel = VoiceRecorderViewModel()
+    
+    var body: some View {
+        Text("Voice Recorder View")
+    }
+}
+
+// MARK: - TitleView
+private struct TitleView: View {
+    fileprivate var body: some View {
+        HStack {
+            Text("음성 메모")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundStyle(Color.customBlack)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.top, 30)
+    }
 }
 
 struct VoiceRecorderView_Previews: PreviewProvider {
