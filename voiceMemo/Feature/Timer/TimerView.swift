4bd43c51ffe33013b7a963c806647fa8fc6652ba
HongBeom <hongbeom7877@naver.com>
2024-12-24 16:45:08 +0900

    [feat] #3 TimerView - TitleView

--- voiceMemo/Feature/Timer/TimerView.swift
+++ voiceMemo/Feature/Timer/TimerView.swift
@@ -6,13 +6,43 @@
 import SwiftUI
 
 struct TimerView: View {
-  var body: some View {
-    Text("Timer")
-  }
+    @StateObject var timeViewModel = TimerViewModel()
+    
+    var body: some View {
+        Text("Timer")
+    }
+}
+
+// MARK: - SetTimerView
+private struct SetTimerView: View {
+    @ObservedObject private var timeViewModel: TimerViewModel
+    
+    fileprivate init(timeViewModel: TimerViewModel) {
+        self.timeViewModel = timeViewModel
+    }
+    
+    fileprivate var body: some View {
+        Text("SetTimerView")
+    }
+}
+
+// MARK: - TitleView
+private struct TitleView: View {
+    fileprivate var body: some View {
+        HStack {
+            Text("타이머")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundStyle(Color.customBlack)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.top, 30)
+    }
 }
 
 struct TimerView_Previews: PreviewProvider {
-  static var previews: some View {
-    TimerView()
-  }
+    static var previews: some View {
+        TimerView()
+    }
 }
