8d79af6c402412bf625913b39afa62168013c712
HongBeom <hongbeom7877@naver.com>
2024-12-24 15:55:56 +0900

    [fix] #3 add Service - NotificationDelegate

--- voiceMemo/App/AppDelegate.swift
+++ voiceMemo/App/AppDelegate.swift
@@ -5,6 +5,14 @@
 
 import UIKit
 
-class AppDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate{ // NSObject: Objective-C의 클래스를 상속받아 사용할 수 있게 해주는 클래스
+    var notificationDelegate = NotificationDelegate()
+    
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+            UNUserNotificationCenter.current().delegate = notificationDelegate
+            return true
+    }
   
 }
