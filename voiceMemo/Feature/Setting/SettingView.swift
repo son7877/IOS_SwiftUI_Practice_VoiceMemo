73b90866f912e288d860c225ad1f6c42c5e0cce8
HongBeom <hongbeom7877@naver.com>
2025-01-06 16:18:11 +0900

    [feat] #5 SettingView - titleView

--- voiceMemo/Feature/Setting/SettingView.swift
+++ voiceMemo/Feature/Setting/SettingView.swift
@@ -6,8 +6,24 @@
 import SwiftUI
 
 struct SettingView: View {
+    @EnvironmentObject private var homeViewModel: HomeViewModel
+    
     var body: some View {
         Text("Setting")
+    }
+}
+
+private struct TitleView: View {
+    fileprivate var body: some View {
+        HStack {
+            Text("설정")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundStyle(Color.customBlack)
+            
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.top, 20)
     }
 }
 
