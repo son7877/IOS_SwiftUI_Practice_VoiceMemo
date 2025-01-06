6fb2264032363fbc4e6eb28f2e8600b878392675
HongBeom <hongbeom7877@naver.com>
2025-01-06 16:51:15 +0900

    [feat] #5 SettingView -totalTabCountView, tabCountView

--- voiceMemo/Feature/Setting/SettingView.swift
+++ voiceMemo/Feature/Setting/SettingView.swift
@@ -13,6 +13,7 @@ struct SettingView: View {
     }
 }
 
+// MARK: - TitleView
 private struct TitleView: View {
     fileprivate var body: some View {
         HStack {
@@ -25,6 +26,53 @@ private struct TitleView: View {
         .padding(.horizontal, 30)
         .padding(.top, 20)
     }
+}
+
+// MARK: - TotalTabCountView
+private struct TotalTabCountView: View {
+    @EnvironmentObject private var homeViewModel: HomeViewModel
+    
+    fileprivate var body: some View {
+        HStack {
+            TabCountView(title: "To do", count: homeViewModel.todosCount)
+            
+            Spacer()
+                .frame(width: 70)
+            
+            TabCountView(title: "Memo", count: homeViewModel.memoCount)
+            
+            Spacer()
+                .frame(width: 70)
+            
+            TabCountView(title: "Voice Recorder", count: homeViewModel.voiceRecordersCount)
+        }
+    }
+}
+
+// MARK: - 각 탭 설정된 카운트 뷰 (공통 뷰 컴포넌트)
+private struct TabCountView: View {
+  private var title: String
+  private var count: Int
+  
+  fileprivate init(
+    title: String,
+    count: Int
+  ) {
+    self.title = title
+    self.count = count
+  }
+  
+  fileprivate var body: some View {
+    VStack(spacing: 5) {
+      Text(title)
+        .font(.system(size: 14))
+        .foregroundColor(.customBlack)
+      
+      Text("\(count)")
+        .font(.system(size: 30, weight: .medium))
+        .foregroundColor(.customBlack)
+    }
+  }
 }
 
 struct SettingView_Previews: PreviewProvider {
